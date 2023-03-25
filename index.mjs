437759506377e4d4a072a2c523fe7ef31193357a
Mitchell Sawatzky <s@watz.ky>
2023-03-25 00:55:36 -0600

    Fix index.mjs

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
-export {APIWrapper} from './src/base.mjs';
+export {WSAuthentication} from './src/authentication.mjs';
 export {WSAPI} from './src/index.mjs';
-export * as exceptions from './src/exceptions.mjs';
-export * as endponts from './src/endpoints.mjs';
 export {GraphQL} from './src/graphql.mjs';
 export {WSSocket} from './src/websocket.mjs';
+export * as exceptions from './src/exceptions.mjs';
+export * as endpoints from './src/endpoints.mjs';
